eddb4618a03819c3ab919a9818c4d6b797854462
Juliana <jflorez@proton.me>
2024-02-25 22:48:59 -0500

    comments and deleteorder

--- CreateApiFunction.js
+++ CreateApiFunction.js
@@ -1,7 +1,7 @@
-/* Please create a React Toolkit Query createApi function call
-that implements the two endpoints that you find in this gist:
-https://gist.github.com/FerdinandvHagen/ce5a939da313c6a8dfe2ad006fd11d9c
-*/
+// Please create a React Toolkit Query createApi function call
+// that implements the two endpoints that you find in this gist:
+// https://gist.github.com/FerdinandvHagen/ce5a939da313c6a8dfe2ad006fd11d9c
+
 
 // First, we import the functions we will need from redux toolkit
 
@@ -11,48 +11,49 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const BASE_URL = 'https://gist.github.com/FerdinandvHagen/ce5a939da313c6a8dfe2ad006fd11d9c';
 
-const api = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: BASE_URL,
-  }),
-  tagTypes: ['Order'],
-  endpoints: (build) => ({
-    // The query accepts a number and returns a Post
-    getOrder: build.query({
-      // note: an optional `queryFn` may be used in place of `query`
-      query: (id) => ({ url: `/orders/{id}` }),
-      // Pick out data and prevent nested properties in a hook or selector
-      transformResponse: (response, meta, arg) => response.data,
-      // Pick out errors and prevent nested properties in a hook or selector
-      transformErrorResponse: (response, meta, arg) => response.status,
-      providesTags: (result, error, id) => [{ type: 'Post', id }],
-      // The 2nd parameter is the destructured `QueryLifecycleApi`
-      async onQueryStarted(
-        arg,
-        {
-          dispatch,
-          getState,
-          extra,
-          requestId,
-          queryFulfilled,
-          getCacheEntry,
-          updateCachedData,
-        }
-      ) {},
-      // The 2nd parameter is the destructured `QueryCacheLifecycleApi`
-      async onCacheEntryAdded(
-        arg,
-        {
-          dispatch,
-          getState,
-          extra,
-          requestId,
-          cacheEntryRemoved,
-          cacheDataLoaded,
-          getCacheEntry,
-          updateCachedData,
-        }
-      ) {},
+// It's time to create the order API
+const orderApi = createApi({
+    baseQuery: fetchBaseQuery({
+        baseUrl: BASE_URL,
     }),
-  }),
+    tagTypes: ['Order'],
+    endpoints: (build) => ({
+        
+        getOrder: build.query({
+            // Defines the endpoint URL for retrieving an order by its ID
+            query: (id) => ({ url: `/orders/${id}` }),
+
+            // Extract relevant information from the response
+            transformResponse: (response, meta, arg) => response.data,
+
+            // Extracts relevant information in case of an error
+            transformErrorResponse: (response, meta, arg) => response.status,
+
+            // specify which tags we will be accessible later using the cache
+            providesTags: (result, error, id) => [{ type: 'order', id }],
+
+        }),
+    }),
+
+    // The mutation accepts an id and Deletes a post, returning a status
+    deleteOrder: build.mutation({
+        query: (id) => ({
+            // Defines the endpoint URL for retrieving an order by its ID
+            url: `/orders/${id}`,
+            method: 'DELETE',
+        }),
+        // Extract relevant information from the response
+        // even though we know the response will be just a status, it's better to beware of any mistakes
+        transformResponse: (response) => response.data,
+
+       // Extracts relevant information in case of an error
+        transformErrorResponse: (response) => response.status,
+
+       // specify which tags we will be invalidated from the cache
+        invalidatesTags: ['order'],
+    }),
 })
+
+export const {
+    useGetOrderQuery, useDeleteOrderMutation
+} = orderApi;
